eec6b6f0671059663a5a17209eac74159a08370c
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-01-09 23:19:25 +0530

    done making the window of the player

---
+++ Main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+class musicplayer:
+    def __init__(self , Tk):
+        self.root = Tk
+        self.root.title('Sugoi Music')
+        self.root.geometry("450x400")
+        root.configure(background='Sky blue')
+root = Tk()
+obj = musicplayer(root)
+root.mainloop()
