e464e6d8405d35f9cdf528ac62757986428f64ed
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-01-10 21:25:54 +0530

    added label

--- Main.py
+++ Main.py
@@ -7,7 +7,10 @@ class musicplayer:
         self.root.geometry("450x400")
         root.configure(background='Sky blue')
         self.bgimg = ImageTk.PhotoImage(file="Imagebg.png")
-        photo = Label(self.root,image=self.bgimg).place(x=100,y=20)
+        photo = Label(self.root,image=self.bgimg).place(x=100,y=40)
+        self.msg = "Let's rock☆*: .｡. o(≧▽≦)o .｡.:*☆"
+
+        self.lab = Label(text=self.msg , fg="white" , bg="black").place(x=130 , y=5)
 root = Tk()
 obj = musicplayer(root)
 root.mainloop()
