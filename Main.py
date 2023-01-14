49e83d5368e90a02ece79a9c952e1323f2a96de6
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-01-14 21:15:58 +0530

    done with the project

--- Main.py
+++ Main.py
@@ -1,11 +1,21 @@
 from tkinter import *
+import os
 from PIL import Image , ImageTk
-
+from tkinter import messagebox
+from tkinter import filedialog
 from pygame import mixer 
+from mutagen.mp3 import MP3
+
 mixer.init()
 
 class musicplayer:
     def __init__(self , Tk):
+        global musicinfo 
+        musicinfo = "Let's rock☆*: .｡. o(≧▽≦)o .｡.:*☆"
+
+        def Openfile():
+            global filename
+            filename = filedialog.askopenfile()
         def mute():
             mixer.music.set_volume(0)
             self.mutes = Button(self.root,image=self.mute,bd=0, command=unmute , background="sky blue").place(x=220,y=300)
@@ -17,11 +27,12 @@ class musicplayer:
                 Paused
             except NameError:
                 try:
-                    mixer.music.load("sample.mp3")
+                    mixer.music.load(filename)
                     mixer.music.play()
                     self.labl1['text'] = "Music Playing"
+                    print(filename)
                 except:
-                    pass
+                    messagebox.showerror('Error',"An Error ocurroed in opning the file")
             else:
                 mixer.music.unpause()
                 self.labl1['text'] = "Music Playing"
@@ -36,14 +47,15 @@ class musicplayer:
         def volumefun(vol):
             volume = int(vol)/100
             mixer.music.set_volume(volume)
+        def About():
+            messagebox.showinfo('About me','This Music Player was build by Kunal Chauhan')
         self.root = Tk
         self.root.title('Sugoi Music')
         self.root.geometry("450x400")
         root.configure(background='Sky blue')
         self.bgimg = ImageTk.PhotoImage(file="Imagebg.png")
         photo = Label(self.root,image=self.bgimg).place(x=100,y=40)
-        self.msg = "Let's rock☆*: .｡. o(≧▽≦)o .｡.:*☆"
-        self.lab = Label(text=self.msg , fg="white" , bg="black").place(x=130 , y=5)
+        self.lab = Label(text=musicinfo , fg="white" , bg="black").place(x=130 , y=5)
         self.labl1 = Label(self.root,text="Let's play it !!", bg="black" , fg="white")
         self.labl1.pack(side=BOTTOM , fill= X)
         self.playButtonImg = ImageTk.PhotoImage(file="Playbut.png")
@@ -60,6 +72,17 @@ class musicplayer:
         self.unmute = ImageTk.PhotoImage(file="unmute.png")
         self.mute = ImageTk.PhotoImage(file="mute.png")
         volumeStatus = Button(self.root,image=self.unmute,command=mute,bd=0 , background="sky blue").place(x=220,y=300)
+        self.menu = Menu(self.root)
+        self.root.configure(menu=self.menu)
+        self.submenu = Menu(self.menu,tearoff=0)
+        self.menu.add_cascade(label='file',menu=self.submenu)
+        self.submenu.add_cascade(label='Open',command=Openfile)
+        self.submenu.add_cascade(label='Exit',command=self.root.destroy)
+        self.submenu2 = Menu(self.menu,tearoff=0)
+        self.menu.add_cascade(label="Help",menu=self.submenu2)
+        self.submenu2.add_command(label="About",command=About)
+        self.root.resizable(0,0)
+
 root = Tk()
 obj = musicplayer(root)
 root.mainloop()
